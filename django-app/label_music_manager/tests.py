22692b1fdeaf763ab3e5e370286aed21df85786a
nb_ <nb01252@surrey.ac.uk>
2024-12-11 09:24:24 +0000

    final testing

--- django-app/label_music_manager/tests.py
+++ django-app/label_music_manager/tests.py
@@ -33,7 +33,7 @@ class ModelTests(TestCase):
         self.assertEqual(str(album), 'Test Album by Test Artist')
 
     def test_album_future_date_validation(self):
-        # Test validation for release date more than 3 years in the future
+        # Test validation for release date > 3 years 
         with self.assertRaises(ValidationError):
             album = Album(
                 title='Future Album',
@@ -55,7 +55,7 @@ class ModelTests(TestCase):
         self.assertEqual(str(song), 'Test Song')
 
     def test_album_tracklist_item(self):
-        # Test creating an album tracklist item
+        # Test creating an album tracklist 
         album = Album.objects.create(
             title='Test Album',
             artist='Test Artist',
@@ -78,7 +78,7 @@ class ModelTests(TestCase):
 
 class ViewTests(TestCase):
     def setUp(self):
-        # Create test users with different profiles
+        # Test with creating users with different portfolio
         self.artist_user = User.objects.create_user(username='artist', password='12345')
         self.artist_profile = MusicManagerUser.objects.create(
             user=self.artist_user,
@@ -100,7 +100,7 @@ class ViewTests(TestCase):
             user_type='viewer'
         )
 
-        # Create a test album
+        # Test album creation
         self.test_album = Album.objects.create(
             title='Test Album',
             artist='Artist Name',
@@ -119,26 +119,25 @@ class ViewTests(TestCase):
 
     def test_album_create_view_permissions(self):
         # Test album creation permissions
-        # Artist should be able to create
+        # Check if artist can create 
         self.client.login(username='artist', password='12345')
         album_data = {
             'title': 'New Artist Album',
             'price': 9.99,
             'format': 'CD',
             'release_date': date.today(),
-            'tracklist': []  # Empty tracklist for testing
+            'tracklist': [] 
         }
         response = self.client.post(reverse('album-create'), data=album_data)
-        self.assertIn(response.status_code, [200, 302])  # Allow both form render and redirect
+        self.assertIn(response.status_code, [200, 302])  
 
-        # Viewer should not be able to create
+        # Can viewer create 
         self.client.login(username='viewer', password='67890')
         response = self.client.post(reverse('album-create'), data=album_data)
         self.assertRedirects(response, reverse('album-list'))
 
     def test_album_edit_view_permissions(self):
-        # Test album edit permissions
-        # Artist can edit their own album
+        # Test if artists can edit their own albums
         self.client.login(username='artist', password='12345')
         edit_url = reverse('album-edit', args=[self.test_album.id])
         response = self.client.get(edit_url)
@@ -151,7 +150,6 @@ class ViewTests(TestCase):
 
     def test_album_delete_view_permissions(self):
         # Test album delete permissions
-        # Artist can delete their own album
         self.client.login(username='artist', password='12345')
         delete_url = reverse('album-delete', args=[self.test_album.id])
         response = self.client.post(delete_url)
@@ -186,7 +184,7 @@ class APITests(TestCase):
         )
 
     def test_album_api_list(self):
-        # To retrieve list of albums
+        # Test to retrieve list of albums
         response = self.client.get(reverse('album-list'))
         self.assertEqual(response.status_code, 200)
         self.assertIn('API Test Album', str(response.content))
@@ -199,14 +197,14 @@ class APITests(TestCase):
 
 class FormTests(TestCase):
     def setUp(self):
-        # Creating a song for testing
+        # Song creation test
         self.song = Song.objects.create(
             title='Test Song',
             length=180
         )
 
     def test_album_form_valid(self):
-    # Test valid album form submission
+    # Test to check valid album form submission
         form_data = {
             'title': 'New Album',
             'price': 9.99,
@@ -224,12 +222,128 @@ class FormTests(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_album_form_invalid(self):
-        # Test invalid album form submission
+        # Test invalid for album form submission
         form_data = {
             'title': '', 
             'price': 1000,  
             'format': 'XX',  # Invalid format
-            'release_date': date.today() + timedelta(days=1800)  # More than 3 years into the future
+            'release_date': date.today() + timedelta(days=1800)  # More than 3 years into the future so invalid
         }
         form = AlbumForm(data=form_data)
         self.assertFalse(form.is_valid())
+
+
+class AdditionalTests(TestCase):
+    def setUp(self):
+        # Create test users with different profiles
+        self.artist_user = User.objects.create_user(username='artist1', password='12345')
+        self.artist_profile = MusicManagerUser.objects.create(
+            user=self.artist_user,
+            display_name='Artist Name',
+            user_type='artist'
+        )
+
+        # Test dates
+        self.song1 = Song.objects.create(
+            title='Test Song 1',
+            length=180  
+        )
+        self.song2 = Song.objects.create(
+            title='Test Song 2',
+            length=240  
+        )
+
+    def test_album_multiple_songs(self):
+        # Test creating an album with multiple songs
+        album = Album.objects.create(
+            title='Multi-Song Album',
+            artist='Artist Name',
+            price=12.99,
+            format='VL',
+            release_date=date.today()
+        )
+
+        # To add multiple songs to the album
+        AlbumTracklistItem.objects.create(album=album, song=self.song1, position=1)
+        AlbumTracklistItem.objects.create(album=album, song=self.song2, position=2)
+
+        # Check that songs are correctly associated
+        self.assertEqual(album.songs.count(), 2)
+        self.assertIn(self.song1, album.songs.all())
+        self.assertIn(self.song2, album.songs.all())
+            
+
+    def test_album_format_choices(self):
+        # Test album different format choices
+        valid_formats = ['DD', 'CD', 'VL']
+        
+        for fmt in valid_formats:
+            album = Album.objects.create(
+                title=f'Test {fmt} Album',
+                artist='Artist Name',
+                price=9.99,
+                format=fmt,
+                release_date=date.today()
+            )
+            self.assertEqual(album.format, fmt)
+
+    def test_tracklist_unique_constraint(self):
+        # Tracklist's unique constraint testing
+        album = Album.objects.create(
+            title='Unique Tracks Album',
+            artist='Artist Name',
+            price=9.99,
+            format='CD',
+            release_date=date.today()
+        )
+
+        # First addition
+        AlbumTracklistItem.objects.create(album=album, song=self.song1, position=1)
+
+        with self.assertRaises(Exception):
+            AlbumTracklistItem.objects.create(album=album, song=self.song1, position=2)
+
+    def test_user_type_choices(self):
+        # Test user type choices
+        valid_user_types = ['artist', 'editor', 'viewer']
+        
+        for user_type in valid_user_types:
+            user = User.objects.create_user(username=f'test_{user_type}', password='12345')
+            profile = MusicManagerUser.objects.create(
+                user=user,
+                display_name=f'Test {user_type}',
+                user_type=user_type
+            )
+            self.assertEqual(profile.user_type, user_type)
+
+    def test_album_slug_generation(self):
+        # Test for slug
+        album = Album.objects.create(
+            title='Slug Test Album',
+            artist='Artist Name',
+            price=9.99,
+            format='DD',
+            release_date=date.today()
+        )
+
+        # Slug generation
+        self.assertEqual(album.slug, 'slug-test-album-dd')
+
+    def test_song_display_in_album(self):
+        # To create an album and add songs
+        album = Album.objects.create(
+            title='Song Display Album',
+            artist='Artist Name',
+            price=9.99,
+            format='CD',
+            release_date=date.today()
+        )
+
+        # Test to add songs to an album
+        AlbumTracklistItem.objects.create(album=album, song=self.song1, position=1)
+        AlbumTracklistItem.objects.create(album=album, song=self.song2, position=2)
+
+        # Test to check if the songs are displayed correctly
+        self.assertIn(self.song1, album.songs.all())
+        self.assertIn(self.song2, album.songs.all())
+        self.assertEqual(album.songs.count(), 2)
